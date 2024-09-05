59e35e8e21960f0e4c5645db7c855575943aa92e
marton200472 <marton200472@gmail.com>
2024-09-05 22:48:11 +0200

    session handling

--- Models/Session.cs
+++ Models/Session.cs
@@ -1,7 +1,71 @@
 namespace Quaze.Models;
 
+
+
 public class Session
 {
-    public string Id { get; set; }
-    public List<Participant> Participants { get; set; }
+    public string Id { get; private set; }
+
+    public List<Participant> Participants { get; set; } = new();
+
+
+    public int TimeLeft { get; private set; }
+    public int QuestionIndex { get; private set; } = 0;
+    public State CurrentState { get; private set; }
+
+    public event EventHandler StateChanged;
+    public event EventHandler<int> TimerTick;
+
+    public Session(string id, Quiz quiz) {
+        Id = id;
+        this.quiz = quiz;
+    }
+
+    private Quiz quiz;
+    private bool TimerEnabled = false;
+
+    public void OnTimerTick() {
+        if (TimeLeft > 0)
+        {
+            TimeLeft--;
+            TimerTick?.Invoke(this, TimeLeft);
+            return;
+        }
+        else if(TimerEnabled)
+            NextState();
+    }
+
+    private void NextState() {
+        switch (CurrentState)
+        {
+            case State.WaitStart:
+                TimeLeft = quiz.Questions[QuestionIndex].TimeLimit;
+                TimerEnabled = true;
+                CurrentState = State.QuestionActive;
+                break;
+            case State.QuestionActive:
+                CurrentState = State.QuestionEnd;
+                TimerEnabled = false;
+                break;
+            case State.QuestionEnd:
+                if (quiz.Questions.Length>QuestionIndex)
+                {
+                    QuestionIndex++;
+                    TimeLeft = quiz.Questions[QuestionIndex].TimeLimit;
+                    CurrentState = State.QuestionActive;
+                    TimerEnabled = true;
+                }
+                else {
+                    CurrentState = State.End;
+                    TimerEnabled = false;
+                }
+                break;
+        }
+
+        StateChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public enum State {
+        WaitStart, QuestionActive, QuestionEnd, End
+    }
 }
--- SessionService.cs
+++ SessionService.cs
@@ -5,13 +5,13 @@ namespace Quaze;
 public class SessionService : BackgroundService
 {
     public List<Session> Sessions { get; set; } = new();
-    public event EventHandler TimerElapsed;
+    public event EventHandler TimerTick;
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (true)
         {
             await Task.Delay(1000);
-            TimerElapsed?.Invoke(this, EventArgs.Empty);
+            TimerTick?.Invoke(this, EventArgs.Empty);
         }
     }
 }
