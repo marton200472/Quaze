3804c8f645579aaff43e7abdcbff10d37cd996dd
marton200472 <marton200472@gmail.com>
2024-09-08 16:22:50 +0200

    quiz sync basic

--- Models/Session.cs
+++ Models/Session.cs
@@ -1,3 +1,6 @@
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+
 namespace Quaze.Models;
 
 
@@ -11,61 +14,85 @@ public class Session
 
     public int TimeLeft { get; private set; } = 0;
     public int QuestionIndex { get; private set; } = 0;
-    public State CurrentState { get; private set; }
+    public SessionState State { get; private set; }
 
-    public event EventHandler StateChanged;
+    public event EventHandler<SessionState> StateChanged;
     public event EventHandler<int> TimerTick;
 
     public Session(string id, Quiz quiz) {
         Id = id;
-        this.quiz = quiz;
+        Quiz = quiz;
     }
 
-    private Quiz quiz;
+
+    public Quiz Quiz {get; private set;}
     private bool TimerEnabled = false;
 
     public void OnTimerTick() {
+        if (!TimerEnabled)
+        {
+            return;
+        }
+
         if (TimeLeft > 0)
         {
             TimeLeft--;
             TimerTick?.Invoke(this, TimeLeft);
             return;
         }
-        else if(TimerEnabled)
-            NextState();
+        
+        NextState();
     }
 
     private void NextState() {
-        switch (CurrentState)
+        switch (State)
         {
-            case State.WaitStart:
-                TimeLeft = quiz.Questions[QuestionIndex].TimeLimit;
+            case SessionState.WaitStart:
+                TimeLeft = Quiz.Questions[QuestionIndex].TimeLimit;
                 TimerEnabled = true;
-                CurrentState = State.QuestionActive;
+                State = SessionState.QuestionActive;
                 break;
-            case State.QuestionActive:
-                CurrentState = State.QuestionEnd;
+            case SessionState.QuestionActive:
+                State = SessionState.QuestionEnd;
                 TimerEnabled = false;
                 break;
-            case State.QuestionEnd:
-                if (quiz.Questions.Count>QuestionIndex)
+            case SessionState.QuestionEnd:
+                if (Quiz.Questions.Count>QuestionIndex+1)
                 {
                     QuestionIndex++;
-                    TimeLeft = quiz.Questions[QuestionIndex].TimeLimit;
-                    CurrentState = State.QuestionActive;
+                    TimeLeft = Quiz.Questions[QuestionIndex].TimeLimit;
+                    State = SessionState.QuestionActive;
                     TimerEnabled = true;
                 }
                 else {
-                    CurrentState = State.End;
+                    State = SessionState.End;
                     TimerEnabled = false;
                 }
                 break;
         }
 
-        StateChanged?.Invoke(this, EventArgs.Empty);
+        StateChanged?.Invoke(this, State);
     }
 
-    public enum State {
+    public void StartQuiz() {
+        if (State != SessionState.WaitStart)
+        {
+            return;
+        }
+        NextState();
+    }
+
+    public void NextQuestion() {
+        if (State != SessionState.QuestionEnd)
+        {
+            return;
+        }
+        NextState();
+    }
+
+    
+}
+
+public enum SessionState {
         WaitStart, QuestionActive, QuestionEnd, End
     }
-}
