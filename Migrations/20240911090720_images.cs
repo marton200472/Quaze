daf36ac87f6a9aec12e3ecc2128e57d4f07251d0
marton200472 <marton200472@gmail.com>
2024-09-11 11:10:34 +0200

    added db migration for image upload

---
+++ Migrations/20240911090720_images.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quaze.Migrations
+{
+    /// <inheritdoc />
+    public partial class images : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ImageGuid",
+                table: "Questions",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageGuid",
+                table: "Questions");
+        }
+    }
+}
