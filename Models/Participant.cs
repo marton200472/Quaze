6db16aadd09ee470932093879ecbdab8160fbc86
Scumbush <vazulvargalaboczki@gmail.com>
2024-09-19 12:05:48 +0200

    Listing points

--- Models/Participant.cs
+++ Models/Participant.cs
@@ -4,4 +4,5 @@ public class Participant
 {
     public string Name { get; set; }
     public Dictionary<int, List<string>> Answers { get; set; } = new();
+    public int Points { get; set; } = 0;
 }
